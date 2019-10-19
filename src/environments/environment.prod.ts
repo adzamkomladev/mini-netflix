4dde84232046bed9613924996a5d8af3779df87b
adzamkomladev <adzamkomla.dev@gmail.com>
2019-10-19 19:52:36 +0000

    add firebase config and movies api keys and url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  moviesApiUrl: 'http://www.omdbapi.com/',
+  moviesApiKey: '91375bce',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBg0y-0vbrtel1UArghw2pE7rNXXE44mWw',
+    authDomain: 'mini-netfilx-ea024.firebaseapp.com',
+    databaseURL: 'https://mini-netfilx-ea024.firebaseio.com',
+    projectId: 'mini-netfilx-ea024',
+    storageBucket: 'mini-netfilx-ea024.appspot.com',
+    messagingSenderId: '563036990317',
+    appId: '1:563036990317:web:17cee5d8ee503081df8864',
+    measurementId: 'G-GYC49EBTGV'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  moviesApiUrl: 'http://www.omdbapi.com/',
+  moviesApiKey: '91375bce',
+  firebaseConfig: {
+    apiKey: 'AIzaSyBg0y-0vbrtel1UArghw2pE7rNXXE44mWw',
+    authDomain: 'mini-netfilx-ea024.firebaseapp.com',
+    databaseURL: 'https://mini-netfilx-ea024.firebaseio.com',
+    projectId: 'mini-netfilx-ea024',
+    storageBucket: 'mini-netfilx-ea024.appspot.com',
+    messagingSenderId: '563036990317',
+    appId: '1:563036990317:web:17cee5d8ee503081df8864',
+    measurementId: 'G-GYC49EBTGV'
+  }
 };
 
 /*
